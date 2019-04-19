a177842d22a3e4e473b61f13c8f430a64f1e4aee
Fatih <fatih.develi@hotmail.com>
2019-04-19 12:41:36 +0300

    Added THE2

---
+++ THE2/simulator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "writeOutput.h"
+#include "pthread.h"
+#include "semaphore.h"
+
+using namespace std;
+// TODO: fix the makefile
+int main() {
+    int Nm; // Number of miners
+    cin >> Nm;
+    for (int i = 0; i < Nm; i++) {
+        cout << "hi" << endl;
+    }
+    
+
+
+    return 0;
+}
